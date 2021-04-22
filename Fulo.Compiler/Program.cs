6479126fe0ea2cd0f19ac38821d8ed0846343c9b
Masood Ahmed <masoodahm@live.com>
2021-04-22 19:35:40 +0500

    Initial Commit

---
+++ Fulo.Compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fulo.Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Fulo.Lang/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fulo.Lang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
