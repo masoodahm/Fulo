c916b8e7592b941dc249124fc2339c0f9b93c71a
Masood Ahmed <masoodahm@live.com>
2021-04-23 22:10:45 +0500

    added code to run and test

--- Fulo.Lang/Program.cs
+++ Fulo.Lang/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Fulo.Lang
 {
     class Program
     {
+        public static string latestErrorMsg = null;
+        private static bool hadError = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 1) {
+                Console.WriteLine("usage fulo [Script.fls]");
+            } else if (args.Length == 1) {
+                runFile(args[0]);
+            }
+        }
+
+        private static void runFile(string path)
+        {
+            if(path.EndsWith(".fls")) {
+                try {
+                    byte[] bytes = File.ReadAllBytes(Path.GetFullPath(path));
+                    string fileName = path;
+                    if (path.Contains("/")) {
+                        fileName = path.Substring(path.LastIndexOf('/') + 1);
+                    }
+                    run(bytes.ToString(), fileName);
+                    if (hadError) Environment.Exit(65);
+                } catch (FileNotFoundException) {
+                    printAndStoreError(path + ": File not Found");
+                } catch (IOException e) {
+                    printAndStoreError(e.Message);
+                }
+            } else {
+                printAndStoreError("Fulo scripts must end with '.fls'.");
+            }
+        }
+
+        private static void run(string source, string fileName)
+        {
+            
+        }
+
+        public static void printAndStoreError(string errorMsg) {
+            latestErrorMsg = errorMsg;
+            Console.WriteLine(errorMsg);
         }
     }
 }
